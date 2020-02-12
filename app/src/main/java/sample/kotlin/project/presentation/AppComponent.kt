ab51b272cdf6c2a714446222cc4bc2d3b872408c
Anna Sentyakova <anna.sent@gmail.com>
2020-02-13 01:35:15 +0400

    Refactoring: DI

--- app/src/main/java/sample/kotlin/project/presentation/AppComponent.kt
+++ app/src/main/java/sample/kotlin/project/presentation/AppComponent.kt
@@ -1,12 +1,7 @@
 package sample.kotlin.project.presentation
 
-import android.content.Context
 import dagger.Component
-import dagger.Module
-import dagger.Provides
 import dagger.android.support.AndroidSupportInjectionModule
-import sample.kotlin.project.data.DataModule
-import sample.kotlin.project.presentation.activities.MainActivityModule
 import javax.inject.Singleton
 
 @Singleton
@@ -19,15 +14,4 @@ import javax.inject.Singleton
 interface AppComponent {
 
     fun inject(app: SkpApp)
-}
-
-@Module(
-    includes = [
-        MainActivityModule::class, DataModule::class
-    ]
-)
-class AppModule(private val context: Context) {
-
-    @Provides
-    fun provideContext() = context
 }
