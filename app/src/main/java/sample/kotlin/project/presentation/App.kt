871b7bf409af980faec601fef183ba2fd425b6c2
Anna Sentyakova <asentyakova@65apps.com>
2020-02-13 18:04:39 +0400

    Added Firebase, Crashlytics, logging; gradle tuning

--- app/src/main/java/sample/kotlin/project/presentation/App.kt
+++ app/src/main/java/sample/kotlin/project/presentation/App.kt
@@ -1,25 +1,38 @@
 package sample.kotlin.project.presentation
 
 import android.app.Application
+import android.content.Context
+import com.crashlytics.android.Crashlytics
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
+import io.fabric.sdk.android.Fabric
+import io.logging.CrashReportSystem
+import io.logging.DumpingUncaughtExceptionHandler
+import io.logging.LogSystem
+import io.logging.utils.DeviceUtils
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import sample.kotlin.project.BuildConfig
-import timber.log.Timber
 import javax.inject.Inject
 
 class App : Application(), HasAndroidInjector {
 
+    private lateinit var logger: Logger
+
     @Inject
     lateinit var androidInjector: DispatchingAndroidInjector<Any>
+
+    override fun androidInjector(): AndroidInjector<Any> = androidInjector
 
     override fun onCreate() {
         super.onCreate()
         setupDagger()
-        setupTimber()
-    }
 
-    override fun androidInjector(): AndroidInjector<Any> = androidInjector
+        // install uncaught exception handler before initializing crash report system
+        initUncaughtExceptionHandler()
+        initLogging()
+    }
 
     private fun setupDagger() {
         DaggerAppComponent.builder()
@@ -28,10 +41,47 @@ class App : Application(), HasAndroidInjector {
             .inject(this)
     }
 
-    private fun setupTimber() {
-        Timber.uprootAll()
-        if (BuildConfig.DEBUG) {
-            Timber.plant(Timber.DebugTree())
+    private fun initUncaughtExceptionHandler() {
+        val dumpOutOfMemory = BuildConfig.DUMP_OUT_OF_MEMORY
+                && DeviceUtils.isExternalStorageWritable()
+        if (!dumpOutOfMemory) {
+            return
         }
+        val externalDir = getExternalFilesDir(null) ?: return
+        val directoryPath = externalDir.absolutePath
+        val handler: Thread.UncaughtExceptionHandler =
+            DumpingUncaughtExceptionHandler(directoryPath)
+        Thread.setDefaultUncaughtExceptionHandler(handler)
+    }
+
+    private fun initLogging() {
+        LogSystem.initLogger(this, LogSystem.Config.builder()
+            .tag(BuildConfig.LOG_TAG)
+            .enableCrs(BuildConfig.ENABLE_CRS)
+            .printLogsToFile(BuildConfig.PRINT_LOGS_TO_FILE)
+            .printLogsToCrs(BuildConfig.PRINT_LOGS_TO_CRS)
+            .printLogsToLogcat(BuildConfig.PRINT_LOGS_TO_LOGCAT)
+            .printSensitiveData(BuildConfig.PRINT_SENSITIVE_DATA)
+            .build(),
+            object : CrashReportSystem {
+                override fun init(
+                    appContext: Context,
+                    enableCrs: Boolean
+                ) {
+                    if (BuildConfig.ENABLE_CRS) {
+                        Fabric.with(appContext, Crashlytics())
+                    }
+                }
+
+                override fun report(throwable: Throwable) {
+                    Crashlytics.logException(throwable)
+                }
+
+                override fun log(msg: String) {
+                    Crashlytics.log(msg)
+                }
+            })
+        logger = LoggerFactory.getLogger(toString())
+        logger.debug("onCreate")
     }
 }
