8bc6f569dc199abd8b0a3e706181fdf977574400
Anna Sentyakova <asentyakova@65apps.com>
2020-02-18 17:51:49 +0400

    Preferences

---
+++ app/src/main/java/sample/kotlin/project/data/preferences/PreferencesConstants.kt
@@ -0,0 +1,10 @@
+package sample.kotlin.project.data.preferences
+
+object PreferencesConstants {
+
+    internal const val PREFERENCES_NAME = "app_preferences"
+
+    private const val PREFIX = "sample.kotlin.project"
+
+    const val KEY_TEST = "$PREFIX.test"
+}
--- app/src/main/java/sample/kotlin/project/data/preferences/PreferencesModule.kt
+++ app/src/main/java/sample/kotlin/project/data/preferences/PreferencesModule.kt
@@ -1,8 +1,11 @@
 package sample.kotlin.project.data.preferences
 
 import android.content.Context
+import android.content.SharedPreferences
+import com.f2prateek.rx.preferences2.RxSharedPreferences
 import dagger.Module
 import dagger.Provides
+import sample.kotlin.project.data.preferences.PreferencesConstants.PREFERENCES_NAME
 import javax.inject.Singleton
 
 @Module
@@ -10,6 +13,12 @@ class PreferencesModule {
 
     @Provides
     @Singleton
-    fun provideSharedPreferences(context: Context) =
-        context.getSharedPreferences("sample.kotlin.project", Context.MODE_PRIVATE)
+    fun provideSharedPreferences(context: Context): SharedPreferences =
+        context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE)
+
+
+    @Singleton
+    @Provides
+    fun provideRxSharedPreferences(sharedPreferences: SharedPreferences) =
+        RxSharedPreferences.create(sharedPreferences)
 }
