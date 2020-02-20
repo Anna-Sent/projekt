566cb73ffc355f4d13ddb70611319612d0b8d5b6
Anna Sentyakova <asentyakova@65apps.com>
2020-02-20 17:10:18 +0400

    Fixed http request

--- app/src/main/java/sample/kotlin/project/data/repositories/search/SearchDataRepository.kt
+++ app/src/main/java/sample/kotlin/project/data/repositories/search/SearchDataRepository.kt
@@ -1,6 +1,7 @@
 package sample.kotlin.project.data.repositories.search
 
 import io.reactivex.Single
+import sample.kotlin.project.domain.providers.schedulers.SchedulersProvider
 import sample.kotlin.project.domain.repositories.search.SearchRepository
 import sample.kotlin.project.domain.sources.request.RequestSource
 import sample.kotlin.project.domain.sources.search.SearchSource
@@ -8,6 +9,7 @@ import javax.inject.Inject
 
 class SearchDataRepository
 @Inject constructor(
+    private val schedulersProvider: SchedulersProvider,
     private val searchSource: SearchSource,
     private val requestSource: RequestSource
 ) : SearchRepository {
@@ -15,6 +17,7 @@ class SearchDataRepository
     override fun search(query: String): Single<String> =
         searchSource.search(query)
             .compose(requestSource.applyStatusUpdating(RequestType.Search))
+            .subscribeOn(schedulersProvider.ioScheduler)
 
     override fun isSearchRunning() = requestSource.status(RequestType.Search)
 }
