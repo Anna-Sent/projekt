245138c975ee2d5dfe9f769912fa370968da5705
Anna Sentyakova <asentyakova@65apps.com>
2020-02-20 12:18:18 +0400

    DI

--- app/src/main/java/sample/kotlin/project/presentation/PresentationModule.kt
+++ app/src/main/java/sample/kotlin/project/presentation/PresentationModule.kt
@@ -1,11 +1,13 @@
 package sample.kotlin.project.presentation
 
 import dagger.Module
+import sample.kotlin.project.domain.DomainModule
 import sample.kotlin.project.presentation.activities.ActivitiesModule
 import sample.kotlin.project.presentation.fragments.FragmentsModule
 
 @Module(
     includes = [
+        DomainModule::class,
         ActivitiesModule::class,
         FragmentsModule::class
     ]
--- app/src/main/java/sample/kotlin/project/presentation/app/AppModule.kt
+++ app/src/main/java/sample/kotlin/project/presentation/app/AppModule.kt
@@ -11,7 +11,6 @@ import javax.inject.Singleton
 
 @Module(
     includes = [
-        DomainModule::class,
         PresentationModule::class
     ]
 )
