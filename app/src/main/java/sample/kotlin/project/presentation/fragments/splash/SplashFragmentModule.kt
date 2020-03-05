efd391875710802b3842e8da2f55c912e89dc017
Anna Sentyakova <asentyakova@65apps.com>
2020-03-05 13:12:48 +0400

    Refactoring

--- app/src/main/java/sample/kotlin/project/presentation/fragments/splash/SplashFragmentModule.kt
+++ app/src/main/java/sample/kotlin/project/presentation/fragments/splash/SplashFragmentModule.kt
@@ -24,7 +24,7 @@ interface SplashFragmentModule {
             ViewModelModule::class
         ]
     )
-    fun bindSearchFragment(): SplashFragment
+    fun bindSplashFragment(): SplashFragment
 
     @Module
     interface Binding {
